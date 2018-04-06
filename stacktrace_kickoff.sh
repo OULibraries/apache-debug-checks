371e35b848cb16c435f4631a16a4aca6de75714f
Cody Bennett <codybennett@ou.edu>
2018-04-06 14:22:42 -0500

    Remove infinite loop to allow Zabbix to handle starting the script

--- stacktrace_kickoff.sh
+++ stacktrace_kickoff.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
-while : ; do
 
-proccount=$(pgrep httpd -c)
-DATE=`date '+%Y-%m-%d %H:%M:%S'`
+DATE=`date '+%Y-%m-%d-%Hh%Mm'`
 
-if [ "$proccount" -gt "90" ]; then
-
-printf "\n----------Begin Strace----------\n" >> highworker_backtrace-$DATE
-strace -o backtrace-$DATE -ffv httpd
-cat backtrace* >> highworker_backtrace-$DATE
-rm -f backtrace*
-printf "\n----------End Strace----------\n"
-printf "\n----------Begin PS----------\n" >> highworker_backtrace-$DATE
-ps axjf >> highworker_backtrace-$DATE
-printf "\n----------End PS----------\n" >> highworker_backtrace-$DATE
-printf "\n----------Begin SS----------\n" >> highworker_backtrace-$DATE
-ss -t -a -m -piostat >> highworker_backtrace-$DATE
-printf "\n----------End SS----------\n" >> highworker_backtrace-$DATE
-printf "\n----------Begin AUSEARCH----------\n" >> highworker_backtrace-$DATE
-ausearch -c httpd -i --debug >> highworker_backtrace-$DATE
-printf "\n----------End AUSEARCH----------\n" >> highworker_backtrace-$DATE
-printf "\n----------Begin Log Tail----------\n" >> highworker_backtrace-$DATE
-tail /var/log/httpd/*log >> highworker_backtrace-$DATE
-printf "\n----------End Log Tail----------\n" >> highworker_backtrace-$DATE
-printf "\n----------Begin LSOF----------\n" >> highworker_backtrace-$DATE
-lsof -u apache >> highworker_backtrace-$DATE
-printf "\n----------End LSOF----------\n" >> highworker_backtrace-$DATE
-printf "\n----------Begin TOP----------\n" >> highworker_backtrace-$DATE
-top -bH -n 1 >> highworker_backtrace-$DATE
-printf "\n----------End TOP----------\n" >> highworker_backtrace-$DATE
-printf "\n----------Begin IPCS----------\n" >> highworker_backtrace-$DATE
-ipcs -a >> highworker_backtrace-$DATE
-printf "\n----------End IPCS----------\n" >> highworker_backtrace-$DATE
-printf "\n----------Begin IPCS Summary----------\n" >> highworker_backtrace-$DATE
-ipcs -u >> highworker_backtrace-$DATE
-printf "\n----------End IPCS Summary----------\n" >> highworker_backtrace-$DATE
-
-fi
-sleep 1
-done
+printf "\n----------Begin Strace----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+strace -o /var/log/apache-debug-checks/backtrace-"${DATE}" -ffv httpd
+cat /var/log/apache-debug-checks/backtrace* >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+rm -f /var/log/apache-debug-checks/backtrace*
+printf "\n----------End Strace----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin PS----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+ps axjf >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End PS----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin SS----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+ss -t -a -m -piostat >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End SS----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin AUSEARCH----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+ausearch -c httpd -i --debug >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End AUSEARCH----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin Log Tail----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+tail /var/log/httpd/*log >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End Log Tail----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin LSOF----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+lsof -u apache >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End LSOF----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin TOP----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+top -bH -n 1 >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End TOP----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin IPCS----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+ipcs -a >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End IPCS----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------Begin IPCS Summary----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+ipcs -u >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
+printf "\n----------End IPCS Summary----------\n" >> /var/log/apache-debug-checks/highworker_backtrace-"${DATE}"
